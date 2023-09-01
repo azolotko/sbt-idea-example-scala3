00cc05bfc2e13cc6a9474af7b4056994b1a624dd
Alex Zolotko <oleksandr.zolotko@ibm.com>
2023-09-01 18:44:10 +0200

    Scala 3 test

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("org.jetbrains" % "sbt-idea-plugin" % "3.20.1")
+addSbtPlugin("org.jetbrains" % "sbt-idea-plugin" % "3.20.3+1-24399fff-SNAPSHOT")
--- src/main/scala/org/jetbrains/scala/samples/ui/MyToolWindowFactory.scala
+++ src/main/scala/org/jetbrains/scala/samples/ui/MyToolWindowFactory.scala
@@ -9,7 +9,10 @@ import org.jetbrains.scala.samples.SamplePluginBundle
 
 class MyToolWindowFactory extends ToolWindowFactory {
 
-  override def createToolWindowContent(project: Project, toolWindow: ToolWindow): Unit =
+  override def createToolWindowContent(project: Project, toolWindow: ToolWindow): Unit = {
+    scala.util.CommandLineParser.showError(scala.util.CommandLineParser.ParseError(0, "hello from Scala 3 std lib"))
+    Console.err.println("hello from Scala 2 std lib ")
     toolWindow.getComponent.add(new JBLabel(SamplePluginBundle.message("my.cool.tool.window")))
+  }
 
 }
